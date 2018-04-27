45b65f2a5f7f35d8f40667a26b920f637a7dc087
LegoMaster616 <andrew.afflitto@gmail.com>
2018-04-27 00:10:50 -0400

    add raspistill command to script

--- glitch.sh
+++ glitch.sh
@@ -3,11 +3,19 @@ EFFECT=$(shuf -n 2 effects.txt | tr -s \\n " ")
 echo $EFFECT
 HLENGTH=160
 
-FILENAME=${1%.bmp}
+if [ -z "$1" ] ; then
+  FILENAME=$(date +"%y%m%d_%H%M")
+else
+  FILENAME=${1%.bmp}
+fi
 
+raspistill -e bmp -0 $FILENAME.bmp -rot 270 -height 1080 -width 1920
+echo "Picture captured"
 sox -V1 -t raw -r 44100 -e mu-law -b 8 -c 1 $FILENAME.bmp -t raw -r 44100 -e mu-law -b 8 -c 1 .soxed $EFFECT
 
-head -c $HLENGTH $FILENAME.bmp > $2
-tail -c +$HLENGTH .soxed >> $2
+head -c $HLENGTH $FILENAME.bmp > $FILENAME"_g.bmp"
+tail -c +$HLENGTH .soxed >> $FILENAME"_g.bmp"
 
 rm .soxed
+mv $FILENAME.bmp webapp/images/original
+mv $FILENAME"_g.bmp" webapp/images/glitched

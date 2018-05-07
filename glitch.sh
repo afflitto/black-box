1ef788530a3c99269a101416dc35eec727a3c209
Andrew Afflitto <andrew@undeadpenguin.com>
2018-05-07 15:27:54 -0400

    add LED status output

--- glitch.sh
+++ glitch.sh
@@ -9,6 +9,7 @@ else
   FILENAME=${1%.bmp}
 fi
 
+gpio set 8 1
 raspistill -e bmp -o $FILENAME.bmp -rot 90 -h 1080 -w 1920 -t 1
 echo "Picture captured"
 sox -V1 -t raw -r 44100 -e mu-law -b 8 -c 1 $FILENAME.bmp -t raw -r 44100 -e mu-law -b 8 -c 1 .soxed $EFFECT
@@ -19,3 +20,4 @@ tail -c +$HLENGTH .soxed >> .output
 rm .soxed
 mv $FILENAME.bmp ~/webapp/images/original
 mv .output ~/webapp/images/glitched/$FILENAME.bmp
+gpio set 8 0

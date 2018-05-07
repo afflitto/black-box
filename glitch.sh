f00436d5743f2a9c79535429843a8812d145f5cc
Andrew Afflitto <andrew@undeadpenguin.com>
2018-05-07 17:57:53 -0400

    add servo support

--- glitch.sh
+++ glitch.sh
@@ -10,8 +10,10 @@ else
 fi
 
 gpio write 8 1
+gpio pwm 1 140
 raspistill -e bmp -o $FILENAME.bmp -rot 90 -h 1080 -w 1920 -t 1
 echo "Picture captured"
+gpio pwm 1 215
 sox -V1 -t raw -r 44100 -e mu-law -b 8 -c 1 $FILENAME.bmp -t raw -r 44100 -e mu-law -b 8 -c 1 .soxed $EFFECT
 
 head -c $HLENGTH $FILENAME.bmp > .output

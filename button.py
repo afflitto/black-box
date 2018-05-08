bd147bf05bbb41823a663d1a5ff37ce808be62ac
Andrew Afflitto <andrew@undeadpenguin.com>
2018-05-07 22:47:07 -0400

    add button listener python script

---
+++ button.py
@@ -0,0 +1,13 @@
+import RPi.GPIO as GPIO
+import os
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(2, GPIO.OUT)
+GPIO.output(2, 0)
+GPIO.PWM(18, 50)
+
+while True:
+    if not GPIO.input(27):
+        os.system("~/black-box/glitch.sh")

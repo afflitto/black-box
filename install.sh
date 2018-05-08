e4249d95cee5d082d9bd83cca5c55dd9dabf966f
Andrew Afflitto <andrew@undeadpenguin.com>
2018-05-08 00:53:04 -0400

    fix install script

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-cd ~/black-box/webapp/images
-mkdir original
-mkdir glitched
+mkdir ~/black-box/webapp/imagesoriginal
+mkdir ~/black-box/webapp/imagesglitched
 
 sudo cp ~/black-box/camera.service /etc/systemd/system
 sudo systemctl enable camera.service

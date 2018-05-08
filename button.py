af4569b3d02705c523fcd28cfc265da8b09b1982
Andrew Afflitto <andrew@undeadpenguin.com>
2018-05-08 00:02:23 -0400

    use bash to setup pwm

--- button.py
+++ button.py
@@ -5,9 +5,7 @@ GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(2, GPIO.OUT)
-GPIO.setup(18, GPIO.OUT)
-GPIO.output(2, 0)
-GPIO.PWM(18, 50)
+os.system("gpio mode 1 pwm; gpio pwm-ms; gpio pwmc 192; gpio pwmr 2000; gpio pwm 1 140")
 
 while True:
     if not GPIO.input(27):
